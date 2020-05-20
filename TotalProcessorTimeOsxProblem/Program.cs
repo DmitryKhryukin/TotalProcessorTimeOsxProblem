ad4c45b1a37b7355a9863d1133aa035a7ff140a6
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2020-05-20 22:16:52 +0300

    project has been added

---
+++ TotalProcessorTimeOsxProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TotalProcessorTimeOsxProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processes = Process.GetProcesses();
+
+            try
+            {
+                var timeInformationToBeUsed = processes.Select(x => x.TotalProcessorTime).ToList();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Hi dear, I'm InvalidOperationException!");
+                Console.WriteLine("Why am I happening? :P");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"StackTrace: {e.StackTrace}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
